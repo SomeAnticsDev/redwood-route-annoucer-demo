80c9af1c4ae1f04a46517c4e727c5baae3f5f9ff
jtoar <dominic.saadi@hey.com>
2021-03-21 11:15:47 -0700

    use react query

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,34 @@
-import { AuthProvider } from '@redwoodjs/auth'
-import netlifyIdentity from 'netlify-identity-widget'
+// import { AuthProvider } from '@redwoodjs/auth'
+// import netlifyIdentity from 'netlify-identity-widget'
 import { FatalErrorBoundary } from '@redwoodjs/web'
-import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+// import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
-import { isBrowser } from '@redwoodjs/prerender/browserUtils'
+// import { isBrowser } from '@redwoodjs/prerender/browserUtils'
+
+// react query
+//------------------------
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { RedwoodReactQueryProvider } from 'redwoodjs-react-query-provider'
+const queryClient = new QueryClient()
 
 import Routes from 'src/Routes'
 
 import './scaffold.css'
 import './index.css'
 
-isBrowser && netlifyIdentity.init()
+// isBrowser && netlifyIdentity.init()
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
-    <AuthProvider client={netlifyIdentity} type="netlify">
-      <RedwoodApolloProvider>
+    {/* <AuthProvider client={netlifyIdentity} type="netlify"> */}
+    {/* <RedwoodApolloProvider> */}
+    <QueryClientProvider client={queryClient}>
+      <RedwoodReactQueryProvider>
         <Routes />
-      </RedwoodApolloProvider>
-    </AuthProvider>
+      </RedwoodReactQueryProvider>
+    </QueryClientProvider>
+    {/* </RedwoodApolloProvider> */}
+    {/* </AuthProvider> */}
   </FatalErrorBoundary>
 )
 
