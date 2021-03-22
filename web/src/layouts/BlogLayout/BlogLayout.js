0f8924dbf61cb14bf8b5a7efa312ff279b1ab46c
jtoar <dominic.saadi@hey.com>
2021-03-22 09:44:35 -0700

    clean up layout

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,9 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
-// import { useAuth } from '@redwoodjs/auth'
 
 const BlogLayout = ({ children }) => {
-  // const { logIn, logOut, isAuthenticated, currentUser } = useAuth()
-
   return (
     <>
       <header className="relative flex justify-between items-center py-4 px-8 bg-blue-700 text-white">
@@ -31,21 +28,7 @@ const BlogLayout = ({ children }) => {
                 Contact
               </Link>
             </li>
-            {/* <li>
-              <a
-                className="py-2 px-4 hover:bg-blue-600 transition duration-100 rounded"
-                href="#"
-                onClick={isAuthenticated ? logOut : logIn}
-              >
-                {isAuthenticated ? 'Log Out' : 'Log In'}
-              </a>
-            </li> */}
           </ul>
-          {/* {isAuthenticated && (
-            <div className="absolute bottom-1 right-0 mr-12 text-xs text-blue-300">
-              {currentUser.email}
-            </div>
-          )} */}
         </nav>
       </header>
       <main className="max-w-4xl mx-auto p-12 bg-white shadow rounded-b">
