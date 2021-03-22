a3a8cb42782b9991c5e54f7a7a18e56a3cad8cb8
jtoar <dominic.saadi@hey.com>
2021-03-22 10:01:51 -0700

    handle direct nav

--- web/src/components/BlogPost/BlogPost.js
+++ web/src/components/BlogPost/BlogPost.js
@@ -1,20 +1,24 @@
 import { Link, routes } from '@redwoodjs/router'
 
-const BlogPost = ({ post, preview = false }) => {
+const BlogPost = ({ post, preview }) => {
   return (
     <article>
       <header>
         {preview ? (
           <h2 className="text-xl text-blue-700 font-semibold">
-            <Link to={routes.blogPost({ slug: post.slug })}>{post.title}</Link>
+            <Link to={routes.blogPost({ slug: post?.slug })}>
+              {post?.title}
+            </Link>
           </h2>
         ) : (
           <h1 className="text-xl text-blue-700 font-semibold">
-            <Link to={routes.blogPost({ slug: post.slug })}>{post.title}</Link>
+            <Link to={routes.blogPost({ slug: post?.slug })}>
+              {post?.title}
+            </Link>
           </h1>
         )}
       </header>
-      <p className="mt-2 text-gray-900 font-light">{post.body}</p>
+      <p className="mt-2 text-gray-900 font-light">{post?.body}</p>
     </article>
   )
 }
